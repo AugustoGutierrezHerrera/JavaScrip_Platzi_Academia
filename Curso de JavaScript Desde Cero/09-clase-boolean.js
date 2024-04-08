be653eaa638748f6d6a15bb9b9e0b86d4594f3b3
afgh <augustogutierrezh@gmail.com>
2024-04-08 07:53:04 -0500

    booleanos y sus conversiones

--- Curso de JavaScript Desde Cero/09-clase-boolean.js
+++ Curso de JavaScript Desde Cero/09-clase-boolean.js
@@ -6,9 +6,14 @@ const hasPermisson=false
 // Conversión Implícita
 const result=5>3
 console.log(result)
+
 const name='Platzi'
 console.log(!!name)
+
+// Conversión explícita (el cero como false y el resto de numeros los toma como true)
 const value=0
 const otherValue=-24
 const explicitBoolean=Boolean(otherValue)
 console.log(explicitBoolean)
+
+//
