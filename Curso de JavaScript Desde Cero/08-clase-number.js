3947d2880d8ffbc32bd580e4c3dff47c2a9368fb
afgh <augustogutierrezh@gmail.com>
2024-04-08 07:48:37 -0500

    manipulacion de number, operaciones y metodos

--- Curso de JavaScript Desde Cero/08-clase-number.js
+++ Curso de JavaScript Desde Cero/08-clase-number.js
@@ -13,18 +13,19 @@ const noEsUnNumero=NaN
 
 //4. Operaiones aritméticas
 //4.1 Suma, resta,multiplicación y división
-const suma=3+4
-const resta=4-4
-const mutiplicacion=4*7
-const division=16/2
+const suma = 3 + 4
+const resta = 4 - 4
+const mutiplicacion = 4 * 7
+const division = 16/2
 //4.2 Modulo y exponenciación
-const modulo=15%8
+const modulo = 15%8
 const exponenciacion=2**3
-
-const resultado=0.1+0.2
+//4.3 Precisión
+const resultado=0.1 + 0.2
 console.log(resultado)
 console.log(resultado.toFixed(1))
 console.log(resultado===0.3)
+//.5 Operaciones avanzadas
 const raizCuadrada=Math.sqrt(16)
 const valorAbsoluto=Math.abs(-7)
 const aleatorio=Math.random()
---
+++ Curso de JavaScript Desde Cero/09-clase-boolean.js
@@ -0,0 +1,14 @@
+//Usar "is"Active como buena practica
+const isActive=true
+//Usar "has"Permission como buena practica
+const hasPermisson=false
+
+// Conversión Implícita
+const result=5>3
+console.log(result)
+const name='Platzi'
+console.log(!!name)
+const value=0
+const otherValue=-24
+const explicitBoolean=Boolean(otherValue)
+console.log(explicitBoolean)
